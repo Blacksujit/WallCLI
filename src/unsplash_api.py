80993edd1a2856e0e7bf96b61d2e9582ad80ce3f
black shadow <nirmalsujit541@gmail.com>
2025-03-25 03:03:08 +0530

    pypi containerise readme✅✅

--- src/unsplash_api.py
+++ src/unsplash_api.py
@@ -11,8 +11,7 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 # Hardcoded NASA APOD API key (replace with your actual key)
-# UNSPLASH_API_KEY = os.getenv('UNSPLASH_API_KEY')
-UNSPLASH_API_KEY = "MvoLPu92DRz6Q2kf1bDsPF2Y7MgoXO5gACIH6PF0oRM"
+UNSPLASH_API_KEY = os.getenv('UNSPLASH_API_KEY')
 
  
 def sanitize_query(query):
