e158e4f0d7306c1fead691df01f56991ea1dfff3
black shadow <nirmalsujit541@gmail.com>
2025-03-25 02:28:51 +0530

    pypi containerise✅✅

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wallpaper-cli-tool",
-    version="1.0.0",
+    version="1.0.1",
     description="A CLI tool to fetch and set beautiful wallpapers from various sources.",
     long_description="""
     # Wallpaper CLI Tool
